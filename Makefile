035044065808619dbfd73dafc9edc50b9bb1a85b
cclaude <cesarclaudepro@gmail.com>
2021-09-01 20:22:25 +0200

    organized project and coded parsing (input is error-proof)

---
+++ Makefile
@@ -0,0 +1,48 @@
+###################################
+# ft_malcolm
+###################################
+
+NAME = ft_malcolm
+
+CC = clang
+
+CFLAGS = -Wall -Wextra -Werror
+
+INCLUDES = -I includes
+
+FILES = ft_malcolm error
+
+SRC = $(addsuffix .c, $(addprefix srcs/, $(FILES))) \
+
+OBJ = $(SRC:c=o)
+
+###################################
+# Methods
+###################################
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	@echo "\033[0;32m\n\nCompiling ft_malcolm..."
+	@$(CC) $(CFLAGS) $(INCLUDES) -o $(NAME) $(OBJ)
+	@echo "\n\033[0mDone !"
+
+%.o: %.c
+	@printf "\033[0;33mGenerating ft_malcolm objects... %-20.20s\r" $@
+	@$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
+
+clean:
+	@echo "\033[0;31m\nDeleting ft_malcolm objects..."
+	@rm -f $(OBJ)
+	@echo "\033[0m"
+
+fclean:
+	@echo "\033[0;31m\nDeleting ft_malcolm objects..."
+	@rm -f $(OBJ)
+	@echo "\nDeleting ft_malcolm executable..."
+	@rm -f $(NAME)
+	@echo "\033[0m"
+
+re: fclean all
+
+.PHONY: clean fclean re
---
+++ includes/ft_malcolm.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_malcolm.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cclaude <cclaude@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/01 19:49:40 by cclaude           #+#    #+#             */
+/*   Updated: 2021/09/01 20:18:15 by cclaude          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_MALCOLM_H
+# define FT_MALCOLM_H
+
+#include <stdio.h>
+#include <arpa/inet.h>
+
+int error_check (int ac, char **av);
+
+#endif
